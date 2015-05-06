1f4d72b240ef1caf0427bd7b20d450338cde6249
phreme <choqq@jadamspam.pl>
2015-05-06 08:35:51 +0200

    sono scanning

--- sensors.h
+++ sensors.h
@@ -1,10 +1,28 @@
 #ifndef SENSORS_H_INCLUDED
 #define SENSORS_H_INCLUDED
 
-const int MinScannerDeclination = 30;
-const int MaxScannerDeclination = 90;
-const int ScanStep = 10;
-int scanResult[2*(MaxScannerDeclination-MinScannerDeclination)/ScanStep+2];
+#include "motor.h"
 
+const int scanResolution = 10;
+const int scanStep = (maxRightAngle - maxLeftAngle) / scanResolution;
+
+Lego_Ultrasonic sono(hSens1);
+
+int sonoScanAndSet() {
+    // returns absolute angle to the farthest obstacle (if any)
+    int max = 0;
+    int at = 0;
+    for (int angle = maxLeftAngle; angle <= maxRightAngle; angle += scanStep) {
+        setSteerAngle(angle);
+        int dist = sono.readDist();
+        if (dist > max) {
+            max = dist;
+            at = angle;
+        }
+        sys.delay_ms(250);
+    }
+    setSteerAngle(at);
+    return at;
+}
 
 #endif // SENSORS_H_INCLUDED
