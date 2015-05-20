375314da7c3154000af77bc9ce3bd3777ae17835
phreme <choqq@jadamspam.pl>
2015-05-20 18:16:51 +0200

    files merger

--- main.cpp
+++ main.cpp
@@ -1,37 +1,140 @@
 /* External devices
-[+] hMot1: drive
-[+] hMot2: steer/sono
-[-] hMot3: elevation
-[!] hSens1: sono (obstacles)
-[-] hSens2: camera (ground roughness)
-[?] hSens3: accel (additional roughness)
+[+] hMot1: drive left
+[+] hMot2: drive right
+[-] hMot3: steer
+[-] hMot4: elevation
+[!] hSens1: sumo eyes (low obstacles)
+[+] hSens2: accelerometer (roughness)
+[-] hSens3: sono (high obstacles)
+[+] LED1: main loop (blinking)
+[-] LED2: lowered suspension
+[-] LED3: obstacle (solid - evadable, blinking - got lost)
 */
 
-#include <stdio.h>
+#include <cstdio>
+#include <cmath>
 
 #include "hFramework.h"
-#include "Lego_Ultrasonic.h"
 #include "hMotor.h"
+#include "Lego_Ultrasonic.h"
+#include "sensors/Hitechnic_Accel.h"
 
 using namespace hFramework;
 using namespace hSensors;
 
-// own headers
-#include "motor.h"
-#include "tests.h"
+const int maxRightAngle = 80;
+const int maxLeftAngle = -maxRightAngle;
+const int scanResolution = 10;
+const int scanStep = (maxRightAngle - maxLeftAngle) / scanResolution;
+
+Lego_Ultrasonic sono(hSens1);
+Hitechnic_Accel accel = Hitechnic_Accel(hSens2);
+float x, y, z;
+int shake;
+
+void setDrivePower(int power) {
+    hMot1.setPower(power);
+    hMot2.setPower(power);
+}
+
+void setSteerAngle(int angle) {
+    hMot3.rotAbs(angle);
+}
+
+void returnSteer() {
+    setSteerAngle(0);
+}
+
+// tests
+void testDrive() {
+    setDrivePower(0);
+    setDrivePower(1000);
+    sys.delay_ms(500);
+    setDrivePower(-1000);
+    sys.delay_ms(500);
+    setDrivePower(0);
+    sys.delay_ms(500);
+}
+
+void testSteer(int angle) {
+    returnSteer();
+    sys.delay_ms(500);
+    setSteerAngle(angle);
+    sys.delay_ms(500);
+    returnSteer();
+    sys.delay_ms(500);
+    setSteerAngle(-angle);
+    sys.delay_ms(500);
+    returnSteer();
+    sys.delay_ms(500);
+}
+
+void testAccel() {
+    hMot1.setPower(shake);
+}
+
+void allTests() {
+    //testDrive();
+    //testSteer(maxRightAngle);
+    //sonoScanAndSet();
+    for(;;) {
+        testAccel();
+        LED2.on(); sys.delay_ms(10); LED2.off(); sys.delay_ms(10);
+    }
+}
+
+// sensors
+
+int sonoScanAndSet() { // wywaliæ
+    // returns absolute angle to the farthest obstacle (if any)
+    int max = 0;
+    int at = 0;
+    for (int angle = maxLeftAngle; angle <= maxRightAngle; angle += scanStep) {
+        setSteerAngle(angle);
+        int dist = sono.readDist();
+        if (dist > max) {
+            max = dist;
+            at = angle;
+        }
+        sys.delay_ms(250);
+    }
+    setSteerAngle(at);
+    return at;
+}
+
+void accelReadShake() {
+    float data[10];
+    for(;;) {
+
+        for (int i = 0; i < 10; i++) {
+            accel.read(x, y, z);
+            data[i] = z;
+            sys.delay_ms(20);
+        }
+        float mean=0.0, sum_deviation=0.0;
+        for (int i = 0; i < 10; ++i)
+        {
+            mean += data[i];
+        }
+        mean /= 10;
+        for (int i = 0; i < 10; ++i)
+            sum_deviation += (data[i] - mean) * (data[i] - mean);
+        shake = (int)(sqrt(sum_deviation/10)*500); // todo: change to actual shake
+    }
+}
 
 void init() {
 	sys.setLogDev(&Serial);
     returnSteer();
+    sys.taskCreate(accelReadShake);
 }
 
 void hMain(void)
 {
+    init();
 	for (;;)
 	{
-	    LED1.on();
-		allTests();
-		LED1.off();
-		sys.delay_ms(2000);
+	    LED1.toggle();
+
 	}
 }
